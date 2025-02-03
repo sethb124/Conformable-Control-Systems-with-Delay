754b1f79532ed44a01943b3ed5e91f808673a933
sethb124 <75157593+sethb124@users.noreply.github.com>
2025-02-03 14:10:06 -0600

    Add files via upload

---
+++ jit_1stord_eq_oneconstdelay_variabihf.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from jitcdde import jitcdde, y, t
+
+def ihf1(t):
+	return [np.exp(-t) - 1]
+
+def ihf2(t):
+	return [np.exp(t) - 1]
+
+ihfs = [ihf1, ihf2]
+
+plt.rcParams['font.size'] = 8
+fig, axs = plt.subplots(2, 2)
+fig.tight_layout(rect=(0, 0, 1, 0.95), pad=3.0)
+fig.suptitle("$y'(t)=-y(t-2)$ solved by jitcdde")
+
+alphas = [1, 0.66, 0.33, 0]
+colors = ['red', 'blue', 'green', 'pink']
+equations = [[-t ** (alpha - 1) * y(0, t - 2.)] for alpha in alphas]
+
+for equation, color, alpha in zip(equations, colors, alphas):
+	dde = jitcdde(equation)
+	for i, rb in enumerate([4, 60]):
+		ts = np.linspace(0, rb, 2000)
+		for j, eqText in enumerate(["e^{{-t}} - 1", "e^{{t}} - 1"]):
+			dde.past_from_function(ihfs[j])
+			dde.integrate_blindly(2)
+			ys = []
+			for t in ts:
+				ys.append(dde.integrate(t))
+			axs[i, j].plot(ts, ys, color=color, linewidth=1, label='$\\alpha=$' + str(alpha))
+			axs[i, j].set_title(f'$ihf(t)={eqText}, t \\in [0, {rb}]$')
+			axs[i, j].legend()
+			axs[i, j].margins(0, 0.1)
+plt.show()
---
+++ jit_1stord_sys_oneconstdelay_constihf.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from jitcdde import jitcdde, y, t
+
+alphas = [1, 0.66, 0.33]
+# alphas = np.linspace(1, 0, 3)
+systems = [[
+	t ** (alpha - 1) * y(1, t),
+	t ** (alpha - 1) * (-0.64 * y(0, t) - 0.16 * y(1, t))]
+for alpha in alphas]
+
+plt.rcParams['font.size'] = 8
+fig, axs = plt.subplots(len(alphas), 1)
+fig.tight_layout(rect=(0, 0, 1, 0.95), pad=3.0)
+
+for i, (system, alpha) in enumerate(zip(systems, alphas)):
+	dde = jitcdde(system, max_delay=100.)
+	ts = np.linspace(0, 20, 2000)
+	dde.constant_past([10, 3]) #, -0.05])
+	dde.integrate_blindly(0.001)
+	ys = []
+	for t in ts:
+		ys.append(dde.integrate(t))
+	ys=np.array(ys)
+	axs[i].plot(ts, ys[:,0], color='blue', linewidth=1, label='Position [m]')
+	axs[i].plot(ts, ys[:,1], color='gold', linewidth=1, label='Velocity [m/s]')
+	axs[i].set_title('$\\alpha=$' + str(alpha))
+	axs[i].legend()
+	axs[i].margins(0, 0.1)
+plt.show()
